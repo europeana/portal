6cb1d289c04817cd34649da47fff8dd8d99a166f
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-05-24 08:21:07 +0000

    Test skeleton for search controller.

---
+++ src/test/java/eu/europeana/portal2/web/controllers/SearchControllerTest.java
@@ -0,0 +1,14 @@
+package eu.europeana.portal2.web.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
