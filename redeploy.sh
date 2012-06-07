c787bab531f5247d62dada8039969a7bf5d5e3df
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-06-07 12:09:53 +0000

    Separate Tomcat directory configuration.

--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+TOMCAT_DIR=~/tomcat/apache-tomcat-7.0.27
+
 kill `ps aux | grep tomcat | grep -v grep | awk '{print $2}'`
-rm -rf ~/tomcat/apache-tomcat-7.0.27/webapps/portal2
-rm ~/tomcat/apache-tomcat-7.0.27/webapps/portal2.war
+rm -rf $TOMCAT_DIR/webapps/portal2
+rm $TOMCAT_DIR/webapps/portal2.war
 mvn clean install
-cp target/portal2-1.0-SNAPSHOT.war ~/tomcat/apache-tomcat-7.0.27/webapps/portal2.war
-~/tomcat/apache-tomcat-7.0.27/bin/startup.sh
+cp target/portal2-1.0-SNAPSHOT.war $TOMCAT_DIR/webapps/portal2.war
+$TOMCAT_DIR/bin/startup.sh
