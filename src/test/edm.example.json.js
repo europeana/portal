c93ecfe6d7f1d8efb21456d38857417aff7706df
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-06-21 15:34:44 +0000

    JSON examples

---
+++ src/test/edm.example.json.js
@@ -0,0 +1,256 @@
+var object1 = [
+		{
+			"_id" : ObjectId("4fba1bc5e4b04d2f9d0f2e5a"),
+			"className" : "eu.europeana.corelib.solr.bean.impl.FullBeanImpl",
+			"about" : "#CM:0159589",
+			"europeanaCompleteness" : 0,
+			"places" : [ {
+				"prefLabel" : {
+					"fr" : "FRANCE"
+				},
+				"altLabel" : {
+					"fr" : "FRANCE / EUROPE"
+				},
+				"latitude" : 0,
+				"longitude" : 0,
+				"about" : "http://sws.geonames.org/3017382/"
+			} ],
+			"concepts" : [ {
+				"prefLabel" : {
+					"def" : "Hautbois musette"
+				},
+				"about" : "http://www.mimo-db.eu/InstrumentsKeywords/5530"
+			} ],
+			"aggregations" : [ {
+				"edmDataProvider" : "Cité de la musique",
+				"edmIsShownBy" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000012212.jpg",
+				"edmIsShownAt" : "http://mediatheque.cite-musique.fr/masc/play.asp?ID=0159589",
+				"edmObject" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000012212.jpg",
+				"edmProvider" : "MIMO - Musical Instrument Museums Online",
+				"edmRights" : "",
+				"hasView" : [ "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000012212.jpg" ],
+				"aggregatedCHO" : "#CM:0159589",
+				"webResources" : [ {
+					"webResourceDcRights" : [ null ],
+					"webResourceEdmRights" : "http://creativecommons.org/licenses/by-nc-sa/3.0/",
+					"about" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000012212.jpg"
+				} ],
+				"about" : "http://www.mimo-db.eu/CM/0159589"
+			} ],
+			"providedCHOs" : [ {
+				"about" : "#CM:0159589"
+			} ],
+			"proxies" : [ {
+				"about" : "#CM:0159589",
+				"dcContributor" : [],
+				"dcCoverage" : [ "http://sws.geonames.org/3017382/" ],
+				"dcCreator" : [ "Anonyme" ],
+				"dcDate" : [ "début 19e" ],
+				"dcDescription" : [ "2 corps en érable * 1 virole en ivoire * (Source : Inventaire du musée) * Collection Geneviève Thibault de Chambure" ],
+				"dcFormat" : [],
+				"dcIdentifier" : [ "#CM:0159589" ],
+				"dcLanguage" : [],
+				"dcPublisher" : [],
+				"dcRelation" : [],
+				"dcRights" : [],
+				"dcSource" : [],
+				"dcSubject" : [],
+				"dcTitle" : [ "Hautbois musette" ],
+				"dcType" : [ "http://www.mimo-db.eu/InstrumentsKeywords/5530" ],
+				"dctermsAlternative" : [],
+				"dctermsConformsTo" : [],
+				"dctermsCreated" : [],
+				"dctermsExtent" : [],
+				"dctermsHasFormat" : [],
+				"dctermsHasPart" : [],
+				"dctermsHasVersion" : [],
+				"dctermsIsFormatOf" : [],
+				"dctermsIsPartOf" : [],
+				"dctermsIsReferencedBy" : [],
+				"dctermsIsReplacedBy" : [],
+				"dctermsIsRequiredBy" : [],
+				"dctermsIssued" : [],
+				"dctermsIsVersionOf" : [],
+				"dctermsMedium" : [],
+				"dctermsProvenance" : [],
+				"dctermsReferences" : [],
+				"dctermsReplaces" : [],
+				"dctermsRequires" : [],
+				"dctermsSpatial" : [],
+				"dctermsTOC" : [],
+				"dctermsTemporal" : [],
+				"edmType" : "IMAGE",
+				"proxyIn" : "http://www.mimo-db.eu/CM/0159589",
+				"proxyFor" : "#CM:0159589"
+			} ]
+		},
+		{
+			"_id" : ObjectId("4fba1bc5e4b04d2f9d0f2e61"),
+			"className" : "eu.europeana.corelib.solr.bean.impl.FullBeanImpl",
+			"about" : "#CM:0159590",
+			"europeanaCompleteness" : 0,
+			"places" : [ {
+				"prefLabel" : {
+					"fr" : "ITALIE"
+				},
+				"altLabel" : {
+					"fr" : "ITALIE / EUROPE"
+				},
+				"latitude" : 0,
+				"longitude" : 0,
+				"about" : "http://sws.geonames.org/3175395/"
+			} ],
+			"concepts" : [
+					{
+						"prefLabel" : {
+							"def" : "Cornett"
+						},
+						"about" : "http://www.mimo-db.eu/InstrumentsKeywords/3868"
+					},
+					{
+						"prefLabel" : {
+							"en" : "423.212 Labrosones with fingerholes with (narrow) conical bore"
+						},
+						"about" : "http://www.mimo-db.eu/HornbostelAndSachs/354"
+					} ],
+			"aggregations" : [ {
+				"edmDataProvider" : "Cité de la musique",
+				"edmIsShownBy" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000013353.jpg",
+				"edmIsShownAt" : "http://mediatheque.cite-musique.fr/masc/play.asp?ID=0159590",
+				"edmObject" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000013353.jpg",
+				"edmProvider" : "MIMO - Musical Instrument Museums Online",
+				"edmRights" : "",
+				"hasView" : [
+						"http://www.mimo-db.eu/media/CM/IMAGE/CMIM000013353.jpg",
+						"http://www.mimo-db.eu/media/CM/IMAGE/CMIM000009846.jpg" ],
+				"aggregatedCHO" : "#CM:0159590",
+				"webResources" : [
+						{
+							"webResourceDcRights" : [ null ],
+							"webResourceEdmRights" : "http://creativecommons.org/licenses/by-nc-sa/3.0/",
+							"about" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000013353.jpg"
+						},
+						{
+							"webResourceDcRights" : [ null ],
+							"webResourceEdmRights" : "http://creativecommons.org/licenses/by-nc-sa/3.0/",
+							"about" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000009846.jpg"
+						} ],
+				"about" : "http://www.mimo-db.eu/CM/0159590"
+			} ],
+			"providedCHOs" : [ {
+				"about" : "#CM:0159590"
+			} ],
+			"proxies" : [ {
+				"about" : "#CM:0159590",
+				"dcContributor" : [],
+				"dcCoverage" : [ "http://sws.geonames.org/3175395/" ],
+				"dcCreator" : [ "Anonyme" ],
+				"dcDate" : [ "16e" ],
+				"dcDescription" : [ "Ivoire * Tête diamantée * 6 + 1 trous * (Source : Inventaire du musée) * D'après Chouquet 'M. Fayet l'a acquis en Italie et l'a donné à Clapisson'. Collection Louis Clapisson. Déposé au Panthéon en [avril] 1940, réintégré à une date inconnue" ],
+				"dcFormat" : [],
+				"dcIdentifier" : [ "#CM:0159590" ],
+				"dcLanguage" : [],
+				"dcPublisher" : [],
+				"dcRelation" : [],
+				"dcRights" : [],
+				"dcSource" : [],
+				"dcSubject" : [],
+				"dcTitle" : [ "Dessus de cornet à bouquin" ],
+				"dcType" : [ "http://www.mimo-db.eu/InstrumentsKeywords/3868",
+						"http://www.mimo-db.eu/HornbostelAndSachs/354" ],
+				"dctermsAlternative" : [],
+				"dctermsConformsTo" : [],
+				"dctermsCreated" : [],
+				"dctermsExtent" : [],
+				"dctermsHasFormat" : [],
+				"dctermsHasPart" : [],
+				"dctermsHasVersion" : [],
+				"dctermsIsFormatOf" : [],
+				"dctermsIsPartOf" : [],
+				"dctermsIsReferencedBy" : [],
+				"dctermsIsReplacedBy" : [],
+				"dctermsIsRequiredBy" : [],
+				"dctermsIssued" : [],
+				"dctermsIsVersionOf" : [],
+				"dctermsMedium" : [],
+				"dctermsProvenance" : [],
+				"dctermsReferences" : [],
+				"dctermsReplaces" : [],
+				"dctermsRequires" : [],
+				"dctermsSpatial" : [],
+				"dctermsTOC" : [],
+				"dctermsTemporal" : [],
+				"edmType" : "IMAGE",
+				"proxyIn" : "http://www.mimo-db.eu/CM/0159590",
+				"proxyFor" : "#CM:0159590"
+			} ]
+		},
+		{
+			"_id" : ObjectId("4fba1bc5e4b04d2f9d0f2e55"),
+			"about" : "http://www.mimo-db.eu/CM/0159588",
+			"aggregatedCHO" : "#CM:0159588",
+			"className" : "eu.europeana.corelib.solr.entity.AggregationImpl",
+			"edmDataProvider" : "Cité de la musique",
+			"edmIsShownAt" : "http://mediatheque.cite-musique.fr/masc/play.asp?ID=0159588",
+			"edmIsShownBy" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000019822.jpg",
+			"edmObject" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000019822.jpg",
+			"edmProvider" : "MIMO - Musical Instrument Museums Online",
+			"edmRights" : "",
+			"hasView" : [
+					"http://www.mimo-db.eu/media/CM/IMAGE/CMIM000019822.jpg",
+					"http://www.mimo-db.eu/media/CM/IMAGE/CMIM000019823.jpg" ],
+			"webResources" : [
+					{
+						"className" : "eu.europeana.corelib.solr.entity.WebResourceImpl",
+						"webResourceDcRights" : [ null ],
+						"webResourceEdmRights" : "http://creativecommons.org/licenses/by-nc-sa/3.0/",
+						"about" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000019822.jpg"
+					},
+					{
+						"className" : "eu.europeana.corelib.solr.entity.WebResourceImpl",
+						"webResourceDcRights" : [ null ],
+						"webResourceEdmRights" : "http://creativecommons.org/licenses/by-nc-sa/3.0/",
+						"about" : "http://www.mimo-db.eu/media/CM/IMAGE/CMIM000019823.jpg"
+					} ]
+		},
+		{
+			"_id" : ObjectId("4fcdc03eccda6d72281d710f"),
+			"className" : "eu.europeana.corelib.solr.bean.impl.FullBeanImpl",
+			"about" : "/91627/FF872F52BDD64229522367BC86495D720CC4B6B6",
+			"europeanaCompleteness" : 10,
+			"aggregations" : [ {
+				"edmDataProvider" : "Västergötlands museum",
+				"edmIsShownAt" : "http://www9.vgregion.se/vastarvet/objekt.aspx?ID=VGM_A29425",
+				"edmObject" : "http://media1.vgregion.se/vastarvet/VGM/Fotobilder/K-bilder 2/23/1M16_A29425.JPG",
+				"edmProvider" : "Swedish Open Cultural Heritage",
+				"about" : "/91627/FF872F52BDD64229522367BC86495D720CC4B6B6"
+			} ],
+			"providedCHOs" : [ {
+				"about" : "/91627/FF872F52BDD64229522367BC86495D720CC4B6B6"
+			} ],
+			"what" : [ "Kulturhistoria" ],
+			"where" : [ "Kulturhistoria" ],
+			"when" : [ "Kulturhistoria" ],
+			"proxies" : [ {
+				"about" : "/91627/FF872F52BDD64229522367BC86495D720CC4B6B6",
+				"dcDescription" : [ "VY ÖVER EN GÅRD\n \n \n Tillv.tid: \n Övriga nr: 44\n Repro nr: \n " +
+						"Registrator: TR\n \n RealNr: 342 Motiv_spec: BONDGÅRD\n" +
+						"RealNr: 241 1 Motiv_spec: BONDGÅRD\n \n Proviens\n Kod: 10\n " +
+						"Yrke: FOTOGRAFNamn: KARLSSON ANDERS\n Land: \n Län: 16\n Kommun: \n " +
+						"Stad: GÖTLUNDA\n By: \n Gård: \n Fastighet: HAGEN\n Adress: \n \n \n Proviens\n " +
+						"Kod: 81\n Yrke: Namn: SKÖVDE MUSEUM\n Land: \n Län: 16\n Kommun: \n Stad: SKÖVDE\n " +
+						"By: \n Gård: \n Fastighet: \n Adress: \n \n \n Proviens\n Kod: 74\n " +
+						"Yrke: Namn: KARLSSON HELGE\n Land: \n Län: 16\n Kommun: \n Stad: GÖTLUNDA\n " +
+						"By: KLACKEN\n Gård: \n Fastighet: \n Adress: \n \n \n Proviens\n Kod: 21\n " +
+						"Yrke: Namn: \n Land: \n Län: 16\n Kommun: \n Stad: GÖTLUNDA\n By: KLACKEN\n " +
+						"Gård: \n Fastighet: \n Adress:" ],
+				"dcIdentifier" : [ "http://kulturarvsdata.se/VGM/media/VGM_A29425" ],
+				"dcRights" : [ "Okänd" ],
+				"dcSource" : [ "Västergötlands museum" ],
+				"dcSubject" : [ "Kulturhistoria" ],
+				"dcType" : [ "Foto" ],
+				"dctermsIssued" : [ "1900-01-01" ],
+				"edmType" : "IMAGE"
+			} ]
+		} 
+]
