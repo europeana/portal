8f332997c0a50f56e11a16d80ccdb880a52b4b8a
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-07-11 22:13:28 +0000

    Organizing sample json files.

---
+++ src/test/00000._UEDIN_701.json.js
@@ -0,0 +1,85 @@
+var record = {
+	"_id" : ObjectId("4fe2efe2e4b0422eccfafc0e"),
+	"className" : "eu.europeana.corelib.solr.bean.impl.FullBeanImpl",
+	"about" : "/00000/_UEDIN_701",
+	"europeanaCompleteness" : 0,
+	"places" : [ {
+		"latitude" : 0,
+		"longitude" : 0,
+		"altitude" : 0,
+		"altLabel" : {
+			"de" : "Duetschland (?)",
+			"def" : "Böhmen/Tschechien/Europa",
+			"fr" : "FRANCE",
+			"en" : "Cologne/Germany"
+		}
+	} ],
+	"concepts" : [ {
+		"prefLabel" : {
+			"def" : "Mouthpiece"
+		},
+		"about" : "http://www.mimo-db.eu/InstrumentsKeywords/6073"
+	} ],
+	"aggregations" : [ {
+		"edmDataProvider" : "University of Edinburgh",
+		"edmIsShownBy" : "http://www.mimo-db.eu/media/UEDIN/IMAGE/0032672d.jpg",
+		"edmIsShownAt" : "http://www.mimo-db.eu/UEDIN/701",
+		"edmObject" : "http://www.mimo-db.eu/media/UEDIN/IMAGE/0032672d.jpg",
+		"edmProvider" : "MIMO - Musical Instrument Museums Online",
+		"edmRights" : "",
+		"hasView" : [ "http://www.mimo-db.eu/media/UEDIN/IMAGE/0032672d.jpg" ],
+		"aggregatedCHO" : "#UEDIN:701",
+		"webResources" : [ {
+			"webResourceDcRights" : [],
+			"webResourceEdmRights" : "http://creativecommons.org/licenses/by-nc-sa/3.0/",
+			"about" : "http://www.mimo-db.eu/media/UEDIN/IMAGE/0032672d.jpg"
+		} ],
+		"about" : "http://www.mimo-db.eu/UEDIN/701"
+	} ],
+	"providedCHOs" : [ {
+		"about" : "#UEDIN:701"
+	} ],
+	"proxies" : [ {
+		"edmType" : "IMAGE",
+		"proxyIn" : "http://www.mimo-db.eu/UEDIN/701",
+		"proxyFor" : "#UEDIN:701",
+		"dcContributor" : [],
+		"dcCoverage" : [ "" ],
+		"dcCreator" : [],
+		"dcDate" : [ "1970 or earlier" ],
+		"dcDescription" : [ "Technical description: german silver; screw cup, enabling depth to be increased by up to 8mm; medium rim contour; maximum bite curvature 2.0; intermediate cup shape; maximum throat curvature 10.Cup volume 3984 (screwed in), 7935 (screwed out); shape quotient 1.197 (screwed in), 1.524 (screwed out)." ],
+		"dcFormat" : [],
+		"dcIdentifier" : [ "#UEDIN:701" ],
+		"dcLanguage" : [],
+		"dcPublisher" : [],
+		"dcRelation" : [],
+		"dcRights" : [],
+		"dcSource" : [],
+		"dcSubject" : [],
+		"dcTitle" : [ "Mouthpiece for tenor trombone.", "" ],
+		"dcType" : [ "http://www.mimo-db.eu/InstrumentsKeywords/6073" ],
+		"dctermsAlternative" : [],
+		"dctermsConformsTo" : [],
+		"dctermsCreated" : [],
+		"dctermsExtent" : [],
+		"dctermsHasFormat" : [],
+		"dctermsHasPart" : [],
+		"dctermsHasVersion" : [],
+		"dctermsIsFormatOf" : [],
+		"dctermsIsPartOf" : [],
+		"dctermsIsReferencedBy" : [],
+		"dctermsIsReplacedBy" : [],
+		"dctermsIsRequiredBy" : [],
+		"dctermsIssued" : [],
+		"dctermsIsVersionOf" : [],
+		"dctermsMedium" : [],
+		"dctermsProvenance" : [],
+		"dctermsReferences" : [],
+		"dctermsReplaces" : [],
+		"dctermsRequires" : [],
+		"dctermsSpatial" : [],
+		"dctermsTOC" : [],
+		"dctermsTemporal" : [],
+		"about" : "#UEDIN:701"
+	} ]
+}
---
+++ src/test/91627.B056315A5C6D63CF55A8735DBAA45884EC3F1ADE.json.js
@@ -0,0 +1,30 @@
+var record = {
+	"_id" : ObjectId("4fcdc03dccda6d72281d70fe"),
+	"className" : "eu.europeana.corelib.solr.bean.impl.FullBeanImpl",
+	"about" : "/91627/B056315A5C6D63CF55A8735DBAA45884EC3F1ADE",
+	"europeanaCompleteness" : 10,
+	"aggregations" : [ {
+		"edmDataProvider" : "Västergötlands museum",
+		"edmIsShownAt" : "http://www9.vgregion.se/vastarvet/objekt.aspx?ID=VGM_A29408",
+		"edmObject" : "http://media1.vgregion.se/vastarvet/VGM/Fotobilder/K-bilder 2/22/1M16_A29408.JPG",
+		"edmProvider" : "Swedish Open Cultural Heritage",
+		"about" : "/91627/B056315A5C6D63CF55A8735DBAA45884EC3F1ADE"
+	} ],
+	"providedCHOs" : [ {
+		"about" : "/91627/B056315A5C6D63CF55A8735DBAA45884EC3F1ADE"
+	} ],
+	"what" : [ "Kulturhistoria" ],
+	"where" : [ "Kulturhistoria" ],
+	"when" : [ "Kulturhistoria" ],
+	"proxies" : [ {
+		"about" : "/91627/B056315A5C6D63CF55A8735DBAA45884EC3F1ADE",
+		"dcDescription" : [ "EXTERIÖR AV BOSTADSHUS\n \n \n Tillv.tid: \n Övriga nr: 44\n Repro nr: \n Registrator: TR\n \n RealNr: 342 Motiv_spec: BOSTADSHUS\n \n Proviens\n Kod: 10\n Yrke: FOTOGRAFNamn: KARLSSON ANDERS\n Land: \n Län: 16\n Kommun: \n Stad: GÖTLUNDA\n By: \n Gård: \n Fastighet: HAGEN\n Adress: \n \n \n Proviens\n Kod: 81\n Yrke: Namn: SKÖVDE MUSEUM\n Land: \n Län: 16\n Kommun: \n Stad: SKÖVDE\n By: \n Gård: \n Fastighet: \n Adress:Proviens\n Kod: 74\n Yrke: Namn: FÄGER\n Land: \n Län: 16\n Kommun: \n Stad: GÖTLUNDA\n By: \n Gård: \n Fastighet: \n Adress: FAGERLUND\n \n \n Proviens\n Kod: 21\n Yrke: Namn: \n Land: \n Län: 16\n Kommun: \n Stad: GÖTLUNDA\n By: \n Gård: \n Fastighet: \n Adress: FAGERLUND" ],
+		"dcIdentifier" : [ "http://kulturarvsdata.se/VGM/media/VGM_A29408" ],
+		"dcRights" : [ "Okänd" ],
+		"dcSource" : [ "Västergötlands museum" ],
+		"dcSubject" : [ "Kulturhistoria" ],
+		"dcType" : [ "Foto" ],
+		"dctermsIssued" : [ "1900-01-01" ],
+		"edmType" : "IMAGE"
+	} ]
+};
