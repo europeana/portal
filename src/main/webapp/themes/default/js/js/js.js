c102028591f395c645f3b7c724cd21957ae07e07
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-07-20 11:17:24 +0000

    test refactor

---
+++ src/main/webapp/themes/default/js/js/js.js
@@ -0,0 +1,15 @@
+function test( elm ) {
+  
+  var i,
+      ii = elm.length,
+      my_variable;
+  
+  for( i = 0; i < ii; i += 1 ) {
+	
+	
+  }
+  
+}
+
+var MyClass = function() {};
+function myFunction() {}
