726b5852c87fecc2775e9f076247cfd215e8b7bb
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-08-02 14:19:03 +0000

    Build api2 as well

--- full-redeploy.sh
+++ full-redeploy.sh
@@ -1,1 +1,13 @@
-cd ../corelib/ && mvn clean install -DskipTests && cd ../portal2 && ./redeploy.sh
+#!/bin/sh
+##############
+# Builds and deploy portal2 and all its dependencies inside Europeana
+##############
+
+# building corelib
+cd ../corelib/ && mvn clean install -DskipTests && cd ../portal2
+
+# building API2
+cd ../api2 && mvn clean install -DskipTests && cd ../portal2
+
+# building portal2 and deploy API2 and portal2
+./redeploy.sh
