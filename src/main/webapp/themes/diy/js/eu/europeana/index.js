ecc0603a042fc8ec1e0660c240356abad997afe1
d_entous@yahoo.com <d_entous@yahoo.com>
2012-08-05 13:04:29 +0000

    starting to play with the idea of js loading carousel items

--- src/main/webapp/themes/diy/js/eu/europeana/index.js
+++ src/main/webapp/themes/diy/js/eu/europeana/index.js
@@ -4,10 +4,59 @@
   
   var index = {
 	
-	carouselInit : function() {
+	carousel_width : $('#index-carousel ul').eq(0).width(),
+	image_width : $('#index-carousel img').eq(0).width(),
+	
+	
+	openCarousel : function() {
 	  
-	  jQuery('#index-carousel').imagesLoaded(function() {
-		this.rCarousel({ item_width_is_container_width : false });
+	  jQuery('#index-carousel').rCarousel({ item_width_is_container_width : false });
+	  
+	},
+	
+	
+	createItemHtml : function( data ) {
+	  
+	  var item,
+		  i,
+		  ii = data.items.length,
+		  html = '';
+	  
+	  for ( i = 0; i < ii; i += 1 ) {
+		
+		item = data.items[i];
+		
+		html +=
+		  '<li>' +
+			'<a href="' + item.anchorUrl + '" title="' + item.anchorTitle + '" target="'+ item.anchorTarget + '">' +
+			  '<img src="' + item.imgUrl + '" alt="' + item.imgAlt + '" width="' + item.imgWidth + '" height="' + item.imgHeight + '"/>' +
+			  '<div class="caption">' +
+				item.anchorTitle +
+			  '</div>' +
+			'</a>' +
+		  '</li>';
+		
+	  }
+	  
+	  return html;
+	  
+	},
+	
+	
+	placeImages : function( data ) {
+	  
+	  $('#index-carousel ul').append( this.createItemHtml( data ) );
+	  this.openCarousel();
+	  
+	},
+	
+	
+	getImages : function() {
+	  
+	  $.ajax({
+		url : '/portal2/carousel.json',
+		data : { start : $('#index-carousel li').length, rows : 2 },
+		success : function( data ) { index.placeImages( data ); }
 	  });
 	  
 	},
@@ -24,7 +73,7 @@
 		file : 'rcarousel.js',
 		path : 'themes/common/js/com/gmtplusone/',
 		dependencies : [ 'jquery.imagesloaded.min.js' ],
-		callback : function() { index.carouselInit(); }
+		callback : function() { index.getImages(); }
 	  }]);
 	  
 	},
@@ -35,6 +84,7 @@
 	  index.loadDependencies();
 	  
 	}
+	
 	
   };
   
