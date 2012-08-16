081f7c46afc3ea9ca2539969c30617de2a9c942d
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-08-16 10:21:53 +0000

    Carousel styling / behaviour / info display

--- src/main/webapp/themes/default/js/galleria/themes/europeana/galleria.europeana.js
+++ src/main/webapp/themes/default/js/galleria/themes/europeana/galleria.europeana.js
@@ -115,14 +115,11 @@ Galleria.addTheme({
 
     this.$('loader').hide();
     
-    
-
 	var containerHeight = parseInt( this.$( 'container' ).css("height") );
-	//alert(containerHeight)
-	
 	var dataSource = this._options.dataSource;
 	
 	/* Action */
+	
 	this.$( 'thumbnails' ).find('.galleria-image').each(function(i, ob){
 		$(ob).unbind('click');
 		$(ob).click(function(e, a){
@@ -133,13 +130,15 @@ Galleria.addTheme({
 
 	});
 	
-	/* Style */
+	
+	/* Styling & info */
+	
 	var navRight	= 	$(".galleria-thumbnails-container .galleria-thumb-nav-right");
 	var navLeft		= 	$(".galleria-thumbnails-container .galleria-thumb-nav-left");
 	navRight.css	("top", (containerHeight - 124)/2 + "px");
 	navLeft.css		("top", (containerHeight - 124)/2 + "px");    	
 
-    var setThumbStyle = function(thumb, thumbOb){
+    var setThumbStyle = function(thumb, thumbOb, index){
     	var tParent	= thumb.parent();
 		var margin = 0;
 		var imgBox = containerHeight-margin;
@@ -149,26 +148,57 @@ Galleria.addTheme({
 		
 		thumb.css("width",		"auto");
 		thumb.css("height",		"auto");
-		thumb.css("max-width",	imgBox + "px");
-		thumb.css("max-height",	imgBox + "px");
+		thumb.css("max-width",	 "100%");
+		thumb.css("max-height",	 "100%");
 		
-		thumbOb.outerWidth = imgBox+7; // this '8' compensates a margin(?) that causes the last element to wrap (with 16 items)
 		
-		// vertical centering
+//		tParent.append('<div class="europeana-carousel-info">Andy Fucking MacLean (' + index + '), Andy Fucking MacLean, Andy Fucking MacLean, Andy Fucking MacLean</div>');
+		tParent.append('<div class="europeana-carousel-info"><div class="title">' + dataSource[index].title + "</div><div>" + dataSource[index].description + '</div></div>');
+		
+		/* Gallery.updateCarousel() looks 1st for property "outerWidth" when calculating the total width of the thumbnail list.
+		 * If the width is too short the last item(s) will wrap and never be viewable.
+		 * If the width is too big there is spare space at the end of the scroll.
+		 * 
+		 * Here we help updateCarousel() by setting "outerWidth" to our box dimension plus the relative component margins and offsets. 
+		 *  */
+		var offset = 2;
+		offset += parseInt( tParent.css("margin-right") );
+		thumbOb.outerWidth = imgBox + offset;
+		
+		/* Vertical centering of individual images */
 		if(imgBox > parseInt(thumb.css("height"))){
 			var top = (imgBox - parseInt(thumb.css("height")) ) / 2;
 			thumb.css("top", top + "px");
 		}
-		thisGallery.updateCarousel();
+		
+		completedCallBackCount ++;	// bump callback counter
+		
+		if(completedCallBackCount == expectedCallBackCount){
+			/* Do this once only: horizontal positioning of the full image strip to centre an image within the viewer  */
+			// this works perfectly at gallery height 260 */
+			
+			var galleryWidth	= $(".galleria-container").width();
+			var offsetLeft		= (galleryWidth / 2) - (imgBox / 2);
+			
+			offsetLeft -= parseInt($(".galleria-carousel").css("left"));
+			offsetLeft -= parseInt($(".galleria-carousel .galleria-thumbnails-list").css("margin-left"));
+
+			$(".galleria-thumbnails").css("margin-left", offsetLeft + "px");
+			thisGallery.updateCarousel();
+		}
     };
+    
+	var expectedCallBackCount = 0;
+	var completedCallBackCount = 0;
 
     $(this._thumbnails).each(function( i, thumb ) {
     	if(thumb.ready){
     	}
     	else{
+    		expectedCallBackCount ++;
     		thisGallery.bind("thumbnail", function(e) {
     			if(e.index == i){
-        	        setThumbStyle($(e.thumbTarget), thumb);
+        	        setThumbStyle($(e.thumbTarget), thumb, i);
     			}
     	    });
     	}
