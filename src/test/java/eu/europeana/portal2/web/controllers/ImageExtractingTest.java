f0548d7e19cc1ca9228175d62410821f2baa0617
jacob.lundqvist@gmail.com <jacob.lundqvist@gmail.com>
2012-08-20 14:38:39 +0000

    utf8 tests in hudson - seems to be working now, removed debug comments

--- src/test/java/eu/europeana/portal2/web/controllers/ImageExtractingTest.java
+++ src/test/java/eu/europeana/portal2/web/controllers/ImageExtractingTest.java
@@ -21,7 +21,6 @@ public class ImageExtractingTest {
 				"width=\"182\" height=\"283\" /></a>" +
 				"<a href=\"http://www.europeana.eu/portal/record/9200103/B67F2D7063672BEDC3CAD554F6494BA84909B26C.html\" " +
 				"target=\"_blank\"><img class=\"alignnone  wp-image-7727\" " +
-				// target=\"_blank\"
 				"title=\"'Napoleon Bonaparte, sketched at Saint Helena by a British Officier in May last', French National Library\" " +
 				"src=\"http://blog.europeana.eu/wp-content/uploads/2012/08/portraitsketch.jpg\" " +
 				"alt=\"'Napoleon Bonaparte, sketched at Saint Helena by a British Officier in May last', French National Library\" " +
