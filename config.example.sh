3251fc0ae7efd2c33fa5ea0f72ef967c3a769ebb
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-08-27 13:32:52 +0000

    Configuration file for command line tools

---
+++ config.example.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# TODO: 
+# 1. cp config.example.sh config.sh
+# 2. enter your Tomcat directory's path into config.sh
+# 3. enter the path of directory where the europeana.properties file(s) takes place
+
+export TOMCAT_DIR=YOUR TOMCAT DIRECTORY
+export PROPERTY_FILE_DIR=/path/to/property/directory
