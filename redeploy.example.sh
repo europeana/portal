5d468e19e616197520d14389c3b7da20a38fba75
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-09-12 10:59:18 +0000

    Makes use of config file

--- redeploy.example.sh
+++ redeploy.example.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-TOMCAT_DIR=~/tomcat/apache-tomcat-7.0.27
+. ./config.sh
 
 kill `ps aux | grep tomcat | grep -v grep | awk '{print $2}'`
+
 rm -rf $TOMCAT_DIR/webapps/portal2
 rm $TOMCAT_DIR/webapps/portal2.war
+rm -rf $TOMCAT_DIR/webapps/api2
+rm $TOMCAT_DIR/webapps/api2-war.war
+
 mvn clean install
+
 cp target/portal2-1.0-SNAPSHOT.war $TOMCAT_DIR/webapps/portal2.war
+
 $TOMCAT_DIR/bin/startup.sh
