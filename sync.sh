624872d20a8bdafbb3401f821db6f8816a1b24f3
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-09-19 12:42:51 +0000

    Syncing branding directory

--- sync.sh
+++ sync.sh
@@ -3,4 +3,5 @@
 . ./config.sh
 
 rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/jsp/ $TOMCAT_DIR/webapps/portal2/WEB-INF/jsp
-rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/tags/ $TOMCAT_DIR/webapps/portal2/WEB-INF/tags
+rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/jsp/ $TOMCAT_DIR/webapps/portal2/WEB-INF/jsp
+rsync -avz --exclude '.svn' src/main/webapp/branding/ $TOMCAT_DIR/webapps/portal2/branding
