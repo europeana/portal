23cc770bd2ef7e29acc4d37de45b0d8d470ef9b4
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-09-24 11:31:26 +0000

    Image controller initial version

---
+++ src/main/java/eu/europeana/portal2/web/controllers/ImageController.java
@@ -0,0 +1,49 @@
+package eu.europeana.portal2.web.controllers;
+
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import eu.europeana.corelib.db.entity.nosql.Image;
+import eu.europeana.corelib.db.entity.nosql.ImageCache;
+import eu.europeana.corelib.db.exception.DatabaseException;
+import eu.europeana.corelib.db.service.ThumbnailService;
+import eu.europeana.corelib.definitions.model.ThumbSize;
+
+@Controller
+public class ImageController {
+
+	@Resource private ThumbnailService thumbnailService;
+
+	@RequestMapping("/image")
+	public byte[] getImage(
+			@RequestParam(value = "url", required = false, defaultValue="*:*") String url,
+			@RequestParam(value = "size", required = false) String size,
+			@RequestParam(value = "embeddedLogo", required = false) String embeddedLogo,
+			HttpServletRequest request, 
+			HttpServletResponse response,
+			Locale locale
+			) {
+		/*
+		 * see ThumbnailServiceTest, MongoImageViewServlet
+		 * */
+		ImageCache cache;
+		try {
+			cache = thumbnailService.findByOriginalUrl(url);
+			Image image = cache.getImages().get(ThumbSize.TINY.toString());
+			image.getHeight();
+			image.getWidth();
+		return image.getImage();
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
