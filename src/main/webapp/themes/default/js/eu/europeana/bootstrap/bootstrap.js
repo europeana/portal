47eb558afbf1defda2c56ccb04e90a18f7393ed5
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2012-10-16 10:48:48 +0000

    Ref. updated jquery ui

--- src/main/webapp/themes/default/js/eu/europeana/bootstrap/bootstrap.js
+++ src/main/webapp/themes/default/js/eu/europeana/bootstrap/bootstrap.js
@@ -46,10 +46,13 @@ var europeana_bootstrap = function(){
 	
 	// array of scripts needed for all pages
 	var scripts = [
-//	  {								file : 'jquery-1.4.4.min.js',	// keep in case map / timeline have 1.4 dependency												path : eu.europeana.vars.branding + '/js/jquery/'	},
 	  {	name  : 'jquery',			file : 'jquery-1.8.1.min.js',													path : eu.europeana.vars.branding + '/js/jquery/'	},
-	  { 							file : 'jquery-ui-1.8.2.custom.min.js',											path : eu.europeana.vars.branding + '/js/jquery/',															dependencies : [ 'jquery' ]	},
-	  {	name : 'utils',				file : 'utils' + js.min_suffix + '.js' + js.cache_helper,						path : eu.europeana.vars.branding + '/js/js/' + js.min_directory,											dependencies : [ 'jquery-ui-1.8.2.custom.min.js' ]	},
+
+	  { name : 'jquery-ui',			file : 'jquery-ui-1.9.0.custom.min.js',											path : eu.europeana.vars.branding + '/js/jquery/',															dependencies : [ 'jquery' ]	},
+	  
+	  {	name : 'utils',				file : 'utils' + js.min_suffix + '.js' + js.cache_helper,						path : eu.europeana.vars.branding + '/js/js/' + js.min_directory,											dependencies : [ 'jquery-ui' ]	},
+	  
+	  
 	  {	name : 'analytics',			file : 'analytics' + js.min_suffix + '.js' + js.cache_helper,					path : eu.europeana.vars.branding + '/js/com/google/analytics/' + js.min_directory,							dependencies : [ 'utils' ]	},
 	  {	name : 'ajax',				file : 'ajax' + js.min_suffix + '.js' + js.cache_helper,						path : eu.europeana.vars.branding + '/js/eu/europeana/' + js.min_directory,									dependencies : [ 'utils' ]	},
 	  
