ac3d3ab5d09c0013f5988354e8bd282a5c2a4839
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-10-18 10:45:26 +0000

    Controlling API limit

--- src/main/java/eu/europeana/portal2/web/controllers/user/AdminApiLimitController.java
+++ src/main/java/eu/europeana/portal2/web/controllers/user/AdminApiLimitController.java
@@ -17,12 +17,9 @@ import org.springframework.web.servlet.ModelAndView;
 import eu.europeana.corelib.db.service.ApiKeyService;
 import eu.europeana.corelib.db.service.UserService;
 import eu.europeana.corelib.definitions.db.entity.relational.ApiKey;
-import eu.europeana.corelib.definitions.db.entity.relational.User;
 import eu.europeana.portal2.services.Configuration;
 import eu.europeana.portal2.web.presentation.PortalPageInfo;
-import eu.europeana.portal2.web.presentation.model.AdminPage;
 import eu.europeana.portal2.web.presentation.model.LimitApiKeyPage;
-import eu.europeana.portal2.web.presentation.model.RegisterApiPage;
 import eu.europeana.portal2.web.util.ClickStreamLogger;
 import eu.europeana.portal2.web.util.ControllerUtil;
 
@@ -34,7 +31,8 @@ import eu.europeana.portal2.web.util.ControllerUtil;
  */
 
 @Controller
-public class AdminController {
+@RequestMapping("/admin/limitApiKey.html")
+public class AdminApiLimitController {
 
 	// @Resource private LocaleInterceptor localeChangeInterceptor;
 	@Resource(name="corelib_db_userService") private UserService userService;
@@ -47,71 +45,44 @@ public class AdminController {
 
 	private final Logger log = Logger.getLogger(getClass().getName());
 
-	@RequestMapping("/admin.html")
-	public ModelAndView adminHandler(
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView limitApiKeyFormHandler(
+			@RequestParam(value = "userId", required = true) long userId,
+			@RequestParam(value = "apiKey", required = true) String apiKeyId,
+			@ModelAttribute("model") LimitApiKeyPage model,
 			HttpServletRequest request,
 			HttpServletResponse response,
 			Locale locale)
 					throws Exception {
-		log.info("==== admin.html ====");
+		log.info("==== /admin/limitApiKey.html ====");
 		config.registerBaseObjects(request, response, locale);
-		// localeChangeInterceptor.preHandle(request, response, this);
-		AdminPage model = new AdminPage();
-		model.setTheme("devel");
 		config.injectProperties(model);
-		model.setUsers(userService.findAll());
 
-		ModelAndView page = ControllerUtil.createModelAndViewPage(model, locale, PortalPageInfo.ADMIN);
+		ApiKey apiKey = apiKeyService.findByID(apiKeyId);
+		model.setApiKey(apiKey.getId());
+		model.setUsageLimit(apiKey.getUsageLimit());
+
+		clickStreamLogger.logUserAction(request, ClickStreamLogger.UserAction.REGISTER_API);
+		ModelAndView page = ControllerUtil.createModelAndViewPage(model, locale, PortalPageInfo.ADMIN_LIMIT_APIKEY);
 		config.postHandle(this, page);
+
 		return page;
 	}
-	
-	@RequestMapping("/admin/removeUser.html")
-	public String removeUserHandler(
-			@RequestParam(value = "id", required = true) int id,
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String limitApiKeyHandler(
+			@ModelAttribute("model") LimitApiKeyPage model,
 			HttpServletRequest request,
 			HttpServletResponse response,
 			Locale locale)
 					throws Exception {
 		log.info("==== admin.html ====");
 
-		User user = userService.findByID(id);
-		userService.remove(user);
+		ApiKey apiKey = apiKeyService.findByID(model.getApiKey());
+		apiKey.setUsageLimit(model.getUsageLimit());
+		apiKeyService.store(apiKey);
 
 		return "redirect:/admin.html";
 	}
 
-	/*
-	@RequestMapping("/admin/blockUser.html")
-	public String blockUserHandler(
-			@RequestParam(value = "id", required = true) int id,
-			HttpServletRequest request,
-			HttpServletResponse response,
-			Locale locale)
-					throws Exception {
-		log.info("==== admin.html ====");
-
-		User user = userService.findByID(id);
-		user.setEnabled(false);
-		userService.store(user);
-
-		return "redirect:/admin.html";
-	}
-	*/
-
-	@RequestMapping("/admin/removeApiKey.html")
-	public String removeApiKeyHandler(
-			@RequestParam(value = "userId", required = true) long userId,
-			@RequestParam(value = "apiKey", required = true) String apiKey,
-			HttpServletRequest request,
-			HttpServletResponse response,
-			Locale locale)
-					throws Exception {
-		log.info("==== admin/removeApiKey.html ====");
-		log.info(String.format("%s, %s", userId, apiKey));
-
-		userService.removeApiKey(userId, apiKey);
-
-		return "redirect:/admin.html";
-	}
 }
