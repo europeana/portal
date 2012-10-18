0ffcc03ca84b8fc6138051470279645f84b783fe
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-10-18 18:07:21 +0000

    Who what

--- src/test/java/eu/europeana/portal2/web/util/MixedTest.java
+++ src/test/java/eu/europeana/portal2/web/util/MixedTest.java
@@ -3,6 +3,7 @@ package eu.europeana.portal2.web.util;
 import static org.junit.Assert.*;
 
 import java.text.MessageFormat;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
@@ -10,9 +11,38 @@ public class MixedTest {
 
 	String UNION_FACETS_FORMAT = "'{'!ex={0}'}'{0}";
 
-	@Test
+	// @Test
 	public void test() {
 		assertEquals("{!ex=COUNTRY}COUNTRY", MessageFormat.format(UNION_FACETS_FORMAT, "COUNTRY"));
 	}
 
+	@Test
+	public void testClearSeeAlso() {
+		assertEquals("Rembrandt Harmensz. van Rĳn", clearSeeAlso("Rembrandt Harmensz. van Rĳn [picture] (1606-1669)"));
+	}
+
+	private String clearSeeAlso(String value) {
+		System.out.println(value);
+		System.out.println(value.matches("(.*?)\\s*$"));
+		String[] patterns = new String[]{"(.*?)\\s*$", "\\[.*?\\])\\s*$"};
+		
+		for (String pattern : patterns) {
+			System.out.println(pattern);
+			// while (value.matches(pattern)) {
+			value = value.replaceAll(pattern, "");
+			value = value.replaceAll("\\s+$", "");
+			System.out.println(value);
+			// }
+		}
+		System.out.println(value);
+		/*
+		Pattern p = Pattern.compile("\\\\\\\\)");
+		System.out.println(p.matcher(value).matches());
+
+
+		System.out.println(value.matches("\\)"));
+		System.out.println(value.matches(" \\([^\\(\\)]+\\)\\s*$"));
+		*/
+		return value;
+	}
 }
