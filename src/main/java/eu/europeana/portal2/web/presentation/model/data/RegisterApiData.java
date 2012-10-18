58d4f69ff5a2663207e15d0645eea7fca7d38933
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-10-18 10:45:52 +0000

    Extending API registration

--- src/main/java/eu/europeana/portal2/web/presentation/model/data/RegisterApiData.java
+++ src/main/java/eu/europeana/portal2/web/presentation/model/data/RegisterApiData.java
@@ -10,6 +10,13 @@ public class RegisterApiData extends SearchPageData {
 	private String apiKey;
 	private String privateKey;
 	private Boolean disclaimer;
+	private String firstName;
+	private String lastName;
+	private String company;
+	private String country;
+	private String phone;
+	private String address;
+	private String website;
 
 	public String getToken() {
 		return token;
@@ -57,5 +64,61 @@ public class RegisterApiData extends SearchPageData {
 
 	public void setDisclaimer(Boolean disclaimer) {
 		this.disclaimer = disclaimer;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
 	}
 }
