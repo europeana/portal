6393bc60f1a0ed710b28158236b80c0a4544d411
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2012-10-26 13:53:38 +0000

    Adding themes to sync script

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 
 . ./config.sh
 
-rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/jsp/ $TOMCAT_DIR/webapps/portal2/WEB-INF/jsp
-rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/tags/ $TOMCAT_DIR/webapps/portal2/WEB-INF/tags
-rsync -avz --exclude '.svn' src/main/webapp/branding/ $TOMCAT_DIR/webapps/portal2/branding
+rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/jsp/ $TOMCAT_DIR/webapps/portal/WEB-INF/jsp
+rsync -avz --exclude '.svn' src/main/webapp/WEB-INF/tags/ $TOMCAT_DIR/webapps/portal/WEB-INF/tags
+rsync -avz --exclude '.svn' src/main/webapp/branding/ $TOMCAT_DIR/webapps/portal/branding
+rsync -avz --exclude '.svn' src/main/webapp/themes/ $TOMCAT_DIR/webapps/portal/themes
