86cb2c4d2f3ea061ca0070c494457ed70b30883d
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2013-01-30 14:41:37 +0000

    Remove accents

--- src/test/java/eu/europeana/portal2/web/controllers/SuggestionControllerTest.java
+++ src/test/java/eu/europeana/portal2/web/controllers/SuggestionControllerTest.java
@@ -348,7 +348,7 @@ public class SuggestionControllerTest {
 	private String[] getWords() {
 		String text = "Toutes nos notions, toute la science, toute la vie pratique elle-meme sont fondees sur la representation"
 				+ " que nous nous faisons des aspects successifs des choses. Notre esprit, aide par nos sens, classe avant tout"
-				+ " celles-ci dans le temps et dans l'espace, qui sont les deux cadres où nous fixons d'abord ce qui nous est"
+				+ " celles-ci dans le temps et dans l'espace, qui sont les deux cadres ou nous fixons d'abord ce qui nous est"
 				+ " sensible dans le monde exterieur. ecrivons-nous une lettre: nous mettons en suscription le lieu et la date."
 				+ " Ouvrons-nous un journal: ce sont ces indications qui y precedent toutes les depeches. Il en est de meme en"
 				+ " tout et pour tout. Le temps et l'espace, la situation des choses et leur epoque apparaissent ainsi comme les"
@@ -362,8 +362,8 @@ public class SuggestionControllerTest {
 				+ " precise qu'apres avoir traverse ces deux filtres superposes que notre entendement interpose: le temps et"
 				+ " l'espace."
 				+ " Ce qui donne aux travaux d'Einstein leur importance, c'est qu'il a montre, comme nous allons voir, que l'idee que nous nous faisions du temps et de l'espace doit etre completement revisee. Si cela est, la science tout entiere,-et avec elle la psychologie,-doit etre refondue. Telle est la premiere partie de l'oeuvre d'Einstein. Mais la ne s'est pas bornee l'action de son profond genie. Si elle n'etait que cela, elle n'eut ete que negative."
-				+ " Apres avoir demoli, apres avoir deblaye nos connaissances de ce qu'on croyait en etre le piedestal inebranlable et qui n'etait, selon lui, qu'un echafaudage fragile masquant les harmonieuses proportions de l'edifice, il a reconstruit. Il a creuse dans le monument de vastes fenetres qui permettent maintenant de jeter un regard emerveille sur les tresors qu'il recele. En un mot, Einstein a d'une part montre, avec une acuite et une profondeur etonnantes, que la base de nos connaissances semble n'etre pas ce qu'on a cru et doit etre refaite avec un nouveau ciment. D'autre part, il a, sur cette base, renouvele, rebâti l'edifice demoli dans ses fondements memes, et lui a donne une forme hardie dont la beaute et l'unite sont grandioses."
-				+ " Il me reste maintenant a tâcher de preciser, d'une maniere concrete et aussi exacte que possible, ces generalites. Mais je dois insister d'abord sur un point qui a une signification considerable: si Einstein s'etait borne a la premiere partie de son oeuvre,-telle que je viens de l'esquisser,-celle qui ebranle les notions classiques de temps et d'espace, il n'aurait point, dans le monde de la pensee, la gloire qui, des aujourd'hui, aureole son nom."
+				+ " Apres avoir demoli, apres avoir deblaye nos connaissances de ce qu'on croyait en etre le piedestal inebranlable et qui n'etait, selon lui, qu'un echafaudage fragile masquant les harmonieuses proportions de l'edifice, il a reconstruit. Il a creuse dans le monument de vastes fenetres qui permettent maintenant de jeter un regard emerveille sur les tresors qu'il recele. En un mot, Einstein a d'une part montre, avec une acuite et une profondeur etonnantes, que la base de nos connaissances semble n'etre pas ce qu'on a cru et doit etre refaite avec un nouveau ciment. D'autre part, il a, sur cette base, renouvele, rebati l'edifice demoli dans ses fondements memes, et lui a donne une forme hardie dont la beaute et l'unite sont grandioses."
+				+ " Il me reste maintenant a tacher de preciser, d'une maniere concrete et aussi exacte que possible, ces generalites. Mais je dois insister d'abord sur un point qui a une signification considerable: si Einstein s'etait borne a la premiere partie de son oeuvre,-telle que je viens de l'esquisser,-celle qui ebranle les notions classiques de temps et d'espace, il n'aurait point, dans le monde de la pensee, la gloire qui, des aujourd'hui, aureole son nom."
 				+ " La chose est d'importance, car la plupart de ceux qui,-en dehors des specialistes purs,-ont ecrit sur Einstein, ont insiste surtout, et souvent exclusivement, sur ce cote en quelque sorte demolisseur de son intervention. Or, on va voir qu'a ce point de vue, Einstein n'a pas ete le premier ni le seul. Il n'a fait qu'aiguiser davantage et enfoncer un peu plus, entre les blocs mal joints de la science classique, le burin que d'autres avant lui, et surtout le grand Henri Poincare, y avaient des longtemps porte. Ensuite il me restera a expliquer, si je puis, le grand, l'immortel titre d'Einstein a la reconnaissance des hommes, qui est, sur cette oeuvre critique, d'avoir reconstruit, reedifie par ses propres forces quelque chose de magnifique et de neuf: et ici, sa gloire est sans partage.";
 		text = text.replaceAll("-", " ").replaceAll("-", " ").replace(".", " ")
 				.replaceAll(",", " ").replaceAll(":", " ").replace(" ", " ")
@@ -416,7 +416,7 @@ public class SuggestionControllerTest {
 			"menswetenschappen", "manniskovetenskap", "humann", "humantudomani", "humanistiset", "inimteadus", 
 			"humanwissenschaften", "des", "still", "licens", "nation", "et", "r", "30", "der", "01", "na", "deutsch", "domain", "republ", 
 			"und", "for", "04", "mid", "physic", "02", "du", "n", "httpwwweuropeanaeurightsrrp", "h", "and", "from", "y", 
-			"print", "national", "bibliothequ", "materi", "fre", "art", "espana", "sa", "18", "inform", "nr", "número", "standort", 
+			"print", "national", "bibliothequ", "materi", "fre", "art", "espana", "sa", "18", "inform", "nr", "numero", "standort", 
 			"aufbewahrung", "aufbewahrungstandort", "record", "pari", "languag", "dede", "imprime", "11", "proprieta", "12", "og", "fur", 
 			"number", "nederland", "di", "architectur", "yn", "late", "it", "yr", "van", "cultura", "object", "voor", "archeologi",
 			"archaeolog", "archiv", "deutschland", "architektur", "museum", "germani", "type", "kingdom", "librari", "monograph", "1911", 
@@ -427,13 +427,13 @@ public class SuggestionControllerTest {
 			"germania", "tyskland", "bundesrepublik", "saksa", "nemecko", "saksamaa", "nemetorszag", "alemanha", 
 			"jerman", "nemecko", "nemcija", "vokietija", "duitschland", "physicalobject", "epiteszet", "item", "kulturhistoria", 
 			"von", "17", "resid", "vacija", "alemanya", "almanya", "alemana", "nemacka", "almayn", "germanio", "njemacka", 
-			"germania", "giriman", "jarmal", "siaman", "gjermani", "jamanì", "dútslân", "gjermania", "jerman", "alamagn", "almaen", 
+			"germania", "giriman", "jarmal", "siaman", "gjermani", "jamani", "dutslan", "gjermania", "jerman", "alamagn", "almaen", 
 			"alimaniya", "germanja", "toitshi", "udachi", "alimanya",
 			"almaniya", "ghearmailt", "daitschland", "ghermaan", "olmoniya", "duc", "tyskland", "ujerumani", "deutan",
-			"tiamana", "ghearmain", "germanujo", "duiska", "jarmalka", "ijalimani", "duutsjlandj", "heremani", "þyskaland", "alemani", 
-			"census", "duiskka", "alemanyi", "eireann", "heireann", "19110402", "cartlann", "naisiúnta", "departement", "el", "with", "m", 
+			"tiamana", "ghearmain", "germanujo", "duiska", "jarmalka", "ijalimani", "duutsjlandj", "heremani", "alemani", 
+			"census", "duiskka", "alemanyi", "eireann", "heireann", "19110402", "cartlann", "naisiunta", "departement", "el", "with", "m", 
 			"bildarchiv", "architektura", "architettura", "het", "sn", "05", "v", "arquitectura", "2005", "arkitektur", "cultur", 
-			"architectuur", "arhitectura", "architektūra", "arhitektura", "architektúra", "arhitektuur", "arkkitehtuuri", "fysisk", 
+			"architectuur", "arhitectura", "architektūra", "arhitektura", "architektura", "arhitektuur", "arkkitehtuuri", "fysisk", 
 			"provid", "not", "digit", "druck", "w", "c", "16", "carta", "06", "le", "madrid", "j", "ou", "kunstgeschicht", "digital", "nl", 
 			"projekt", "cart", "marburg", "herstellung", "dokumentationszentrum", "zero", "propiedad", "is", 
 			"httpcreativecommonsorgpublicdomainzero", "okand", "03", "09", "nc", "this", "specimen", "08", "a", "druk", "fisica", "physiqu",
