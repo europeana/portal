de3d9bd1111235a68f1179c38d8d81f406e48de0
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2013-01-31 15:49:59 +0000

    rights icon update

--- src/main/java/eu/europeana/portal2/querymodel/query/RightsOption.java
+++ src/main/java/eu/europeana/portal2/querymodel/query/RightsOption.java
@@ -20,16 +20,16 @@ public enum RightsOption {
 	CC_BY_SA("http://creativecommons.org/licenses/by-sa/", "CC BY-SA", "icon-cc icon-by icon-sa", true),
 
 	// CC_BY_NC_SA("http://creativecommons.org/licenses/by-nc-sa/", "CC BY-NC-SA", "cc_by_nc_sa.png"),
-	CC_BY_NC_SA("http://creativecommons.org/licenses/by-nc-sa/", "CC BY-NC-SA", "icon-cc icon-by icon-nc icon-sa", true),
+	CC_BY_NC_SA("http://creativecommons.org/licenses/by-nc-sa/", "CC BY-NC-SA", "icon-cc icon-by icon-nceu icon-sa", true),
 
 	// CC_BY_ND("http://creativecommons.org/licenses/by-nd/", "CC BY-ND", "cc_by_nd.png"),
 	CC_BY_ND("http://creativecommons.org/licenses/by-nd/", "CC BY-ND", "icon-cc icon-by icon-nd", true),
 
 	// CC_BY_NC("http://creativecommons.org/licenses/by-nc/", "CC BY-NC", "cc_by_nc.png"),
-	CC_BY_NC("http://creativecommons.org/licenses/by-nc/", "CC BY-NC", "icon-cc icon-by icon-nc", true),
+	CC_BY_NC("http://creativecommons.org/licenses/by-nc/", "CC BY-NC", "icon-cc icon-by icon-nceu", true),
 
 	// CC_BY_NC_ND("http://creativecommons.org/licenses/by-nc-nd/", "CC BY-NC-ND", "cc_by_nc_nd.png"),
-	CC_BY_NC_ND("http://creativecommons.org/licenses/by-nc-nd/", "CC BY-NC-ND", "icon-cc icon-by icon-nc icon-nd", true),
+	CC_BY_NC_ND("http://creativecommons.org/licenses/by-nc-nd/", "CC BY-NC-ND", "icon-cc icon-by icon-nceu icon-nd", true),
 
 	// NOC("http://creativecommons.org/publicdomain/mark/", "Public Domain marked", "noc.png"),
 	NOC("http://creativecommons.org/publicdomain/mark/", "Public Domain marked", "icon-pd", true),
