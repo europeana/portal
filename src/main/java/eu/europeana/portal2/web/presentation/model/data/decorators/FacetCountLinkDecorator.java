602cd9cda8349334d594d98c143e94ea0909062a
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2013-03-04 13:40:31 +0000

    Optimizing FacetCountLinkDecorator

--- src/main/java/eu/europeana/portal2/web/presentation/model/data/decorators/FacetCountLinkDecorator.java
+++ src/main/java/eu/europeana/portal2/web/presentation/model/data/decorators/FacetCountLinkDecorator.java
@@ -30,8 +30,14 @@ public class FacetCountLinkDecorator implements FacetCountLink {
 
 	private final Logger log = Logger.getLogger(getClass().getName());
 
+	private static final String RIGHTS = "RIGHTS";
+	private static final String COUNTRY = "COUNTRY";
+	private static final String LANGUAGE = "LANGUAGE";
+
 	private FacetCountLink facetCountLink;
 	private String type;
+	private String title;
+	private String value;
 
 	public FacetCountLinkDecorator(String type, FacetCountLink facetCountLink) {
 		this.facetCountLink = facetCountLink;
@@ -73,36 +79,45 @@ public class FacetCountLinkDecorator implements FacetCountLink {
 	 * @return
 	 */
 	public String getTitle() {
-		String title = facetCountLink.getValue();
-		if (StringUtils.equals(type, "RIGHTS")) {
-			if (facetCountLink.getRightsOption() != null) {
-				title = facetCountLink.getRightsOption().getRightsText();
+		if (title == null) {
+			if (StringUtils.equals(type, LANGUAGE)) {
+				PortalLanguage language = PortalLanguage.safeValueOf(facetCountLink.getValue());
+				if ((language != null) && (language.getLanguageName() != null)) {
+					title = language.getLanguageName();
+				} else {
+					title = facetCountLink.getValue();
+				}
+			}
+			else {
+				title = facetCountLink.getValue();
+				if (StringUtils.equals(type, RIGHTS)) {
+					if (facetCountLink.getRightsOption() != null) {
+						title = facetCountLink.getRightsOption().getRightsText();
+					}
+				}
+				if (StringUtils.equals(type, COUNTRY)) {
+					if (StringUtils.length(title) <= 3) {
+						title = StringUtils.upperCase(title);
+					} else {
+						title = WordUtils.capitalizeFully(title);
+					}
+				}
+				title = StringUtils.abbreviate(title, 20);
 			}
 		}
-		if (StringUtils.equals(type, "COUNTRY")) {
-			if (StringUtils.length(title) <= 3) {
-				title = StringUtils.upperCase(title);
-			} else {
-				title = WordUtils.capitalizeFully(title);
-			}
-		}
-		if (StringUtils.equals(type, "LANGUAGE")) {
-			PortalLanguage language = PortalLanguage.safeValueOf(facetCountLink.getValue());
-			if ((language != null) && (language.getLanguageName() != null)) {
-				return language.getLanguageName();
-			}
-		}
-		return StringUtils.abbreviate(title, 20);
+		return title;
 	}
 
 	@Override
 	public String getValue() {
-		if (StringUtils.equals(type, "RIGHTS")) {
-			if (facetCountLink.getRightsOption() != null) {
-				return facetCountLink.getRightsOption().getRightsText();
+		if (value == null) {
+			if (StringUtils.equals(type, RIGHTS) && facetCountLink.getRightsOption() != null) {
+				value = facetCountLink.getRightsOption().getRightsText();
+			} else {
+				value = facetCountLink.getValue();
 			}
 		}
-		return facetCountLink.getValue();
+		return value;
 	}
 
 	@Override
