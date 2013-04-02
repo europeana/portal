98c8a55a91d677210748b6c9df31a1218e5b90c7
andyjmaclean@gmail.com <andyjmaclean@gmail.com>
2013-04-02 09:52:42 +0000

    Based showSimilarItems on europeana.properties

--- src/main/webapp/themes/default/js/eu/europeana/fulldoc.js
+++ src/main/webapp/themes/default/js/eu/europeana/fulldoc.js
@@ -309,7 +309,7 @@ eu.europeana.fulldoc = {
 	/**
 	 * Makes the one and only call to eu.europeana.lightbox.init
 	 * */
-	initLightbox : function(url){
+	initLightbox : function(url, initialW, initialH){
 		js.console.log("initLightbox");
 
 		if(!eu.europeana.fulldoc.lightboxOb){
@@ -358,10 +358,18 @@ eu.europeana.fulldoc = {
 			);				
 			
 			eu.europeana.fulldoc.lightboxOb = new eu.europeana.lightbox();
+			
+			
 			eu.europeana.fulldoc.lightboxOb.init(
-					cmp,
-					url,
-					eu.europeana.fulldoc.getLightboxableCount() > 1 ? carouselData : null);
+				{	"cmp"	:	cmp,
+					"src"	:	url,
+					"w"		:	initialW,
+					"h"		:	initialH,
+					"data"	:	eu.europeana.fulldoc.getLightboxableCount() > 1 ?  carouselData : null
+				}
+				
+			);
+			
 			$(".iframe-wrap").empty();
 		}
 		else{
@@ -428,6 +436,11 @@ eu.europeana.fulldoc = {
 				function($images, $proper, $broken){
 
 					if($proper.length==1 && $proper.width() > 200){
+
+						// need to store the real widths here, because by the time fulldoc.js loads the tmp img element may have been removed.
+						
+						var properW = $proper.width();
+						var properH = $proper.height();
 						
 						// Add the markup
 
@@ -443,7 +456,10 @@ eu.europeana.fulldoc = {
 									});
 								}
 								// end new trigger (was previously in lightbox definition file)
-								eu.europeana.fulldoc.initLightbox(carouselData[index ? index : 0].external.url);
+								
+								//alert("proper " + properW+ " / " +  properH );
+								
+								eu.europeana.fulldoc.initLightbox(carouselData[index ? index : 0].external.url, properW, properH);
 								eu.europeana.fulldoc.showExternalTrigger(true, carouselData[index ? index : 0].external.type, gallery);
 							}
 						);
@@ -646,16 +662,21 @@ eu.europeana.fulldoc = {
 	},
 	
 	initBottomCarousel : function(){
-		return;
-		if (typeof carousel2Data != 'undefined') {
+		
+		if(!eu.europeana.vars.isShowSimilarItems){
+			return;
+		}
+		
+		
+		if(typeof carousel2Data != 'undefined'){
 			
 			var carousel2selector = $('#carousel-2-tabbed').is(":visible") ? '#carousel-2-tabbed' : '#carousel-2'; 
 			
 			// 150 too small for iphone: make min height 200
-			$(carousel2selector).css("height", Math.max(200, eu.europeana.fulldoc.getCarousel2Dimensions().h) + "px");
-
-			var x = Galleria.run(carousel2selector, {
-				debug:			js.debug,
+   			$(carousel2selector).css("height", Math.max(200, eu.europeana.fulldoc.getCarousel2Dimensions().h) + "px");
+   	
+   			var x = Galleria.run(carousel2selector, {
+   				debug:			js.debug,
 				transition:		'fadeslide',
 				carousel:		true,
 				carouselSpeed:	1200,
