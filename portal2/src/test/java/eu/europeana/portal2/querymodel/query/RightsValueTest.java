fd6ce882eb6793f8edb39eba2650b9cb014123c6
Kiraly Peter <peter.kiraly@kb.nl>
2013-09-04 14:10:37 +0200

    #663 Refactoring RightsOptionTest and RightsValueTest according to W-J's suggestions.

--- portal2/src/test/java/eu/europeana/portal2/querymodel/query/RightsValueTest.java
+++ portal2/src/test/java/eu/europeana/portal2/querymodel/query/RightsValueTest.java
@@ -2,25 +2,116 @@ package eu.europeana.portal2.querymodel.query;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import eu.europeana.portal2.web.presentation.model.data.submodel.RightsValue;
 
+/**
+ * Testing RightsValue objects
+ */
 public class RightsValueTest {
 
-	@Test
-	public void test() {
-		String edmRight = "http://www.europeana.eu/rights/out-of-copyright-non-commercial/";
-		String portalUrl = "http://localhost:8080/portal";
+	String portalUrl = "http://europeana.eu/portal";
+	String cczeroUrl = "http://creativecommons.org/publicdomain/zero";
+	String orphanUrl = "http://www.europeana.eu/rights/test-orphan-work-test/";
+	String oocUrl = "http://www.europeana.eu/rights/out-of-copyright-non-commercial/";
+	RightsValue cczero;
+	RightsValue orphan;
+	RightsValue ooc;
 
-		RightsValue rightsValue = RightsValue.safeValueByUrl(edmRight, portalUrl);
-		assertEquals("/rights/out-of-copyright-non-commercial/", rightsValue.getRightsUrl());
-		assertEquals(edmRight, rightsValue.getUrl());
-		assertEquals("Out of copyright - non commercial reuse", rightsValue.getRightsText());
-		assertEquals("icon-publicdomain icon-nc", rightsValue.getRightsIcon());
-		assertEquals(false, rightsValue.getRightsShowExternalIcon());
-		assertEquals("http://localhost:8080/portal/rights/out-of-copyright-non-commercial.html",
-				rightsValue.getRelativeUrl());
-		assertEquals(false, rightsValue.isNoc());
+	@Before
+	/**
+	 * Setting up the RightsValue objects
+	 */
+	public void setup() {
+		cczero = RightsValue.safeValueByUrl(cczeroUrl, portalUrl);
+		orphan = RightsValue.safeValueByUrl(orphanUrl, portalUrl);
+		ooc = RightsValue.safeValueByUrl(oocUrl, portalUrl);
+	}
+
+	@Test
+	/**
+	 * Testing all methods of the out of copyright type
+	 */
+	public void testOutOfCopyright() {
+		assertEquals(oocUrl, ooc.getUrl());
+		assertEquals("/rights/out-of-copyright-non-commercial/", ooc.getRightsUrl());
+		assertEquals("Out of copyright - non commercial reuse", ooc.getRightsText());
+		assertEquals("icon-publicdomain icon-nc", ooc.getRightsIcon());
+		assertEquals(false, ooc.getRightsShowExternalIcon());
+		assertEquals("http://europeana.eu/portal/rights/out-of-copyright-non-commercial.html",
+				ooc.getRelativeUrl());
+		assertEquals(false, ooc.isNoc());
+	}
+
+	@Test
+	/**
+	 * Testing the getUrl() method
+	 */
+	public void testGetUrl() {
+		assertEquals(cczeroUrl, cczero.getUrl());
+		assertEquals(orphanUrl, orphan.getUrl());
+		assertEquals(oocUrl, ooc.getUrl());
+	}
+
+	@Test
+	/**
+	 * Testing the getRightsUrl() method
+	 */
+	public void testGetRightsUrl() {
+		assertEquals(cczeroUrl, cczero.getRightsUrl());
+		assertEquals("/rights/test-orphan-work-test/", orphan.getRightsUrl());
+		assertEquals("/rights/out-of-copyright-non-commercial/", ooc.getRightsUrl());
+	}
+
+	@Test
+	/**
+	 * Testing the getRightsText() method
+	 */
+	public void testGetRightsText() {
+		assertEquals("CC0", cczero.getRightsText());
+		assertEquals("Orphan Work", orphan.getRightsText());
+		assertEquals("Out of copyright - non commercial reuse", ooc.getRightsText());
+	}
+
+	@Test
+	/**
+	 * Testing the getRightsIcon() method
+	 */
+	public void testGetRightsIcon() {
+		assertEquals("icon-cczero", cczero.getRightsIcon());
+		assertEquals("icon-unknown", orphan.getRightsIcon());
+		assertEquals("icon-publicdomain icon-nc", ooc.getRightsIcon());
+	}
+
+	@Test
+	/**
+	 * Testing the getRightsShowExternalIcon() method
+	 */
+	public void testGetRightsShowExternalIcon() {
+		assertEquals(true, cczero.getRightsShowExternalIcon());
+		assertEquals(false, orphan.getRightsShowExternalIcon());
+		assertEquals(false, ooc.getRightsShowExternalIcon());
+	}
+
+	@Test
+	/**
+	 * Testing the getRelativeUrl() method
+	 */
+	public void testGetRelativeUrl() {
+		assertEquals("http://creativecommons.org/publicdomain/zero", cczero.getRelativeUrl());
+		assertEquals("http://europeana.eu/portal/rights/test-orphan-work-test.html", orphan.getRelativeUrl());
+		assertEquals("http://europeana.eu/portal/rights/out-of-copyright-non-commercial.html", ooc.getRelativeUrl());
+	}
+
+	@Test
+	/**
+	 * Testing the isNoc() method
+	 */
+	public void testIsNoc() {
+		assertEquals(true, cczero.isNoc());
+		assertEquals(false, orphan.isNoc());
+		assertEquals(false, ooc.isNoc());
 	}
 }
