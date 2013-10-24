921b3aced74d9e77cf33a18ac05cec1cc4cd88d0
Andy MacLean <andyjmaclean@gmail.com>
2013-10-24 15:26:45 +0200

    #426 Use Selenium for Regression

--- portal2-selenium/src/main/java/eu/europeana/portal2/selenium/page/IndexPage.java
+++ portal2-selenium/src/main/java/eu/europeana/portal2/selenium/page/IndexPage.java
@@ -4,9 +4,42 @@ import org.openqa.selenium.WebDriver;
 
 import eu.europeana.portal2.selenium.page.abstracts.Portal2Page;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.prefs.Preferences;
+
+
+import org.ini4j.Ini;
+import org.ini4j.IniPreferences;
+
+
 public class IndexPage extends Portal2Page {
 	
-	public static final String PAGE = "http://www.europeana.eu/portal/"; 
+	
+    static String url;
+    static String urlSuffix;
+    static String port;
+	static {
+		try{
+			Ini ini = new Ini( new FileReader(new File("conf.ini")) );
+			Preferences prefs = new IniPreferences( ini );
+			
+			url			= prefs.node("server").get("url",        "http://www.europeana.eu/");
+			urlSuffix	= prefs.node("server").get("url-suffix", "/portal");
+			port		= prefs.node("server").get("port",       "");
+		}
+		catch(Exception e){
+			url			= "http://www.europeana.eu/";
+			urlSuffix	= "/portal";
+			port		= "";
+		}
+		
+	}
+	
+	// PAGE defaults to "http://www.europeana.eu/portal/";
+	
+	final static String PAGE = url + (port.length() > 0 ? (":" + port) : "") + urlSuffix;
+   
 	
 	public static IndexPage openPage(WebDriver driver) {
 		driver.get(PAGE);
