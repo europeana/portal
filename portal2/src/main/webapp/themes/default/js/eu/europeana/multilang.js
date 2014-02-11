c4a3cb59c8a331307374c2d640e91ee7e58fda99
dan-nl <d_entous@yahoo.com>
2014-02-10 18:06:36 -0800

    re: #1202 - multilang : banner
    * moved jquery.cookie.js from themes/default to themes/common
    * adjusted multilang.js so that it sets the matching cookie name in the backend, portalLanguage

--- portal2/src/main/webapp/themes/default/js/eu/europeana/multilang.js
+++ portal2/src/main/webapp/themes/default/js/eu/europeana/multilang.js
@@ -12,11 +12,10 @@
 			var hash = this.href.substring( this.href.indexOf( '#' ) );
 
 			if ( hash === '#yes' ) {
-				$.cookie( 'browser-language-selection', eu.europeana.vars.browser_locale, { path: '/' } );
+				$.cookie( 'portalLanguage', eu.europeana.vars.browser_locale, { path: '/' } );
 			} else {
-				$.cookie( 'browser-language-selection', eu.europeana.vars.locale, { path: '/' } );
+				$.cookie( 'portalLanguage', eu.europeana.vars.locale, { path: '/' } );
 			}
-
 		},
 
 		init: function() {
