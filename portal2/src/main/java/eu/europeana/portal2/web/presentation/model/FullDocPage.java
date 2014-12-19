42336405c6b0e48b02a8d0cf2208373874156668
Andy MacLean <andyjmacLean@gmail.com>
2014-12-19 14:24:46 +0100

    Re: #1784 - copied / manual merge of front-end translation changes

--- portal2/src/main/java/eu/europeana/portal2/web/presentation/model/FullDocPage.java
+++ portal2/src/main/java/eu/europeana/portal2/web/presentation/model/FullDocPage.java
@@ -761,12 +761,4 @@ public class FullDocPage extends FullDocPreparation {
 		return null;
 	}
 	
-	public String getBingToken() {
-		return bingToken;
-	}
-
-	public void setBingToken(String bingToken) {
-		this.bingToken = bingToken;
-	}
-
 }
