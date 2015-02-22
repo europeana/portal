dfd9f6a80cfa671c8fd07585e55f89a795ac9c24
Bram Lohman <bram@lohmanict.nl>
2015-02-22 23:35:26 +0100

    Implemented (and configured) pool for handling Jedis resources

--- portal2/src/main/java/eu/europeana/portal2/web/util/keyvalue/RedisProvider.java
+++ portal2/src/main/java/eu/europeana/portal2/web/util/keyvalue/RedisProvider.java
@@ -2,31 +2,61 @@ package eu.europeana.portal2.web.util.keyvalue;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
- * Provides a Java Redis (Jedis) implementation for writing items to cache 
+ * Provides a Java Redis (Jedis) implementation for writing items to cache
  * 
  * @author Bram Lohman
  */
 public class RedisProvider {
 
-  private Jedis jedis;
+	JedisPool pool;
 
-  public RedisProvider(String host, int port, String password) {
-    jedis = new Jedis(host, port);
-    jedis.auth(password);
-    JedisPool pool = new JedisPool("europeana");
-    jedis.setDataSource(pool);
-    jedis.connect();
-  }
+	public RedisProvider(String host, int port, String password) {
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
 
+		// 'Borrowed' from http://www.ncolomer.net/2011/07/time-to-redis/
+		// Tests whether connection is dead when connection
+		// retrieval method is called
+		poolConfig.setTestOnBorrow(true);
+		/* Some extra configuration */
+		// Tests whether connection is dead when returning a
+		// connection to the pool
+		poolConfig.setTestOnReturn(true);
+		// Number of connections to Redis that just sit there
+		// and do nothing
+		poolConfig.setMaxIdle(5);
+		// Minimum number of idle connections to Redis
+		// These can be seen as always open and ready to serve
+		poolConfig.setMinIdle(1);
+		// Tests whether connections are dead during idle periods
+		poolConfig.setTestWhileIdle(true);
+		// Maximum number of connections to test in each idle check
+		poolConfig.setNumTestsPerEvictionRun(10);
+		// Idle connection checking period
+		poolConfig.setTimeBetweenEvictionRunsMillis(60000);
 
-  /**
-   * Getter
-   * @return Jedis
-   */
-  public Jedis getJedis() {
-    return this.jedis;
-  }
+		// Create the jedisPool
+		pool = new JedisPool(poolConfig, host, port, 300, password);
+	}
+
+	/**
+	 * Request a jedis resource from the resource pool 
+	 * 
+	 * @return Jedis
+	 */
+	public Jedis getJedis() {
+		Jedis jedis = pool.getResource();
+		return jedis;
+	}
+	
+	/**
+	 * Return the jedis resource to the resource pool
+	 * @param jedis The jedis resource
+	 */
+	public void returnJedis(Jedis jedis) {
+        pool.returnResource(jedis);
+    }
 
 }
