185d8872e3820adca16132b0e372d2a0fcee4a9b
SAlexandru <salexandru.alex@gmail.com>
2015-03-24 16:38:12 +0100

    fix

--- portal2/src/main/java/eu/europeana/corelib/solr/service/mock/SearchServiceMockSelenium.java
+++ portal2/src/main/java/eu/europeana/corelib/solr/service/mock/SearchServiceMockSelenium.java
@@ -250,9 +250,4 @@ public class SearchServiceMockSelenium implements SearchService {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-    @Override
-    public Integer search(Integer integer, String s, String s2, Boolean aBoolean, Boolean aBoolean2, String s3, String s4, Boolean aBoolean3, String s5, Boolean aBoolean4, String s6) {
-        throw new NotImplementedException();
-    }
 }
